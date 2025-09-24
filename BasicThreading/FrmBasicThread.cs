c1577885effe392630def4f4a043df741dc87cea
Janel Galisfrancisco <janelfrancisco2122@gmail.com>
2025-09-24 19:55:49 +0800

    ThreadA and ThreadB was created

--- BasicThreading/FrmBasicThread.cs
+++ BasicThreading/FrmBasicThread.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,9 @@ namespace BasicThreading
 {
     public partial class FrmBasicThread : Form
     {
+        // 4. In the FrmBasicThread class, create two (2) threads named ThreadA and ThreadB
+        private Thread ThreadA;
+        private Thread ThreadB;
         public FrmBasicThread()
         {
             InitializeComponent();
@@ -20,6 +24,20 @@ namespace BasicThreading
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void FrmBasicThread_Load(object sender, EventArgs e)
+        {
+            ThreadA = new Thread(MyThreadClass.Thread1);
+            ThreadB = new Thread(MyThreadClass.Thread1);
+
+            ThreadA.Name = "Thread A";
+            ThreadB.Name = "Thread B";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            
         }
     }
 }
